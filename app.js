90ceb18ed2aae2b7b7ce5c7ae822ce17fdc2c602
Ilona <ilonarus@hotmail.com>
2021-02-26 16:04:41 +0100

    Added if statement for no more profiles

--- app.js
+++ app.js
@@ -34,18 +34,23 @@ document.getElementById('next').addEventListener('click', nextProfile);
 function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  document.getElementById('profileDisplay').innerHTML = `
-    <ul class="list-group">
-      <li class="list-group-item">Name: ${currentProfile.name}</li>
-      <li class="list-group-item">Age: ${currentProfile.age}</li>
-      <li class="list-group-item">Location: ${currentProfile.location}</li>
-      <li class="list-group-item">Preference: ${currentProfile.gender} looking for a ${currentProfile.lookingfor}</li>
-    </ul>
-  `;
+  if (currentProfile !== undefined) { 
 
-  document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
-} 
+    document.getElementById('profileDisplay').innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Preference: ${currentProfile.gender} looking for a ${currentProfile.lookingfor}</li>
+      </ul>
+    `;
 
+    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
+}
 
 
 // Profile iterator
