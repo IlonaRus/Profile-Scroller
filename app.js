21a07c86c634ed27ef39d197c6e0966f36084fef
Ilona <ilonarus@hotmail.com>
2021-02-26 15:41:17 +0100

    Added data

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Amsterdam',
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+  },
+  {
+    name: 'Jn Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Rotterdam',
+    image: 'https://randomuser.me/api/portraits/women/33.jpg'
+  },
+  {
+    name: 'William Johnson',
+    age: 39,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Utrecht',
+    image: 'https://randomuser.me/api/portraits/men/65.jpg'
+  },
+];
